9093645a16e8610a233c17bb12c22643d31a32c2
markbeg <mark.beg@fermata.tech>
2024-12-06 16:55:12 +0100

    /exchange-token's response now has necessary CORS headers.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.router import router as auth_router
 
@@ -6,6 +7,15 @@ app = FastAPI(
     title="Token Exchange Service",
     description="Service for Hortiview external user authorization",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST", "OPTIONS"],
+    allow_headers=["*"],
+    # max_age=3600,  # Cache preflight requests for 1 hour
 )
 
 app.include_router(auth_router)
